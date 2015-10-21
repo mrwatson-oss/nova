a164da2afae8b13eb8cc7c42b872d1cacd20b1eb
Marius de Corte <mdecorte@gmail.com>
2015-10-21 16:19:16 +0200

    added active tab on horizontal nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
       'use strict';
       $(this).toggleClass("drop-nav-active");
   });
+// make horizontal nav item active on click
+  $('.nav--horizontal li a').on( "click", function() {
+    $(this).parent().siblings().children().removeClass('nav--active');
+    $(this).addClass('nav--active');
+  });
 });
